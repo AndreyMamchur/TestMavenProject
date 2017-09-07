47848e27518cc43a8ea64304db46d3c51ef37667
AndreyMamchur <mamchurandrey87@gmail.com>
2017-09-07 14:36:47 +0300

    test maven

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+import org.joda.time.LocalDate;
+
+/**
+ * Created by ELRIK on 07.09.2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+    }
+}
